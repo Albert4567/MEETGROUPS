6377a65b0d6134aabf3b684237df3fdb86608c3e
mbarbaro09 <mbarbaro@studenti.uninsubria.it>
2021-04-06 15:49:23 +0200

    modificato nome package UI

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/home/HomeViewModel.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/home/HomeViewModel.kt
@@ -1,4 +1,4 @@
-package com.pdm.meetgroups.view.ui.home
+package com.pdm.meetgroups.view.navbar.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/notifications/NotificationsViewModel.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/notifications/NotificationsViewModel.kt
@@ -1,13 +1,13 @@
-package com.pdm.meetgroups.view.ui.dashboard
+package com.pdm.meetgroups.view.navbar.notifications
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
