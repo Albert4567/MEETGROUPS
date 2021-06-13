f14600f1634c2eff14ece02107158f3fd6ec5da9
Alberto <albibogoni@gmail.com>
2021-06-13 22:08:53 +0200

    Creazione PostCreationFragment.

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/viewmodel/JournalViewModel.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/viewmodel/JournalViewModel.kt
@@ -1,16 +1,19 @@
 package com.pdm.meetgroups.viewmodel
 
+import android.view.View
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.Navigation
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.GeoPoint
+import com.pdm.meetgroups.R
 import com.pdm.meetgroups.model.POST_STATUS
 import com.pdm.meetgroups.model.Post
 
 typealias PostList = ArrayList<Post>
 
-// TODO(AB): Insert firestore repository and set it up
 class JournalViewModel : ViewModel() {
     private val posts: MutableLiveData<PostList> = MutableLiveData() // (AB): Temporary code
 
@@ -34,4 +37,17 @@ class JournalViewModel : ViewModel() {
     }
 
     fun getPosts(): LiveData<PostList> = posts
+
+    fun showPostCreationFragment(activity: FragmentActivity) {
+        val navController = activity.let { Navigation.findNavController(it, R.id.nav_host_fragment) }
+        navController.navigate(R.id.action_navigation_journal_to_navigation_post_creation)
+    }
+
+    fun showEditJournalFragment(activity: FragmentActivity) {
+
+    }
+
+    fun showGroupPartecipants(view: View) {
+
+    }
 }
---
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/viewmodel/PostCreationViewModel.kt
@@ -0,0 +1,37 @@
+package com.pdm.meetgroups.viewmodel
+
+import android.view.View
+import android.widget.RadioButton
+import androidx.lifecycle.ViewModel
+import com.pdm.meetgroups.R
+import com.pdm.meetgroups.model.POST_STATUS
+
+class PostCreationViewModel : ViewModel() {
+    lateinit var title: String
+    lateinit var tags: ArrayList<String> // TODO(AB): Define default tags
+    lateinit var visibility: POST_STATUS
+    lateinit var images: ArrayList<String>
+
+    init {
+
+    }
+
+    fun setPostVisibility(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+
+            when (view.getId()) {
+                R.id.radio_post_creation_private ->
+                    if (checked)
+                        visibility = POST_STATUS.PRIVATE
+                R.id.radio_post_creation_public ->
+                    if (checked)
+                        visibility = POST_STATUS.PRIVATE
+            }
+        }
+    }
+
+    fun publishPost() {
+
+    }
+}
