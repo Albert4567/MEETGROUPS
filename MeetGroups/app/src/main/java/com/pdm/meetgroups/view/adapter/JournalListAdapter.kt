03737d25f6a91ab68895f1a425ef2bb99a6ca5ea
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-15 11:32:18 +0200

    Update JournalListAdapter.kt

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/JournalListAdapter.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/JournalListAdapter.kt
@@ -5,8 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.pdm.meetgroups.R
+import com.pdm.meetgroups.viewmodel.JournalViewModelImpl
+import com.pdm.meetgroups.viewmodel.ProfileViewModel
 
-class JournalListAdapter : RecyclerView.Adapter<JournalListAdapter.ViewHolder>() {
+class JournalListAdapter(val profileVM: ProfileViewModel) : RecyclerView.Adapter<JournalListAdapter.ViewHolder>() {
 
     inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
 
@@ -17,10 +19,15 @@ class JournalListAdapter : RecyclerView.Adapter<JournalListAdapter.ViewHolder>()
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return profileVM.getJournals().value!!.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val journalView = inflater.inflate(R.layout.row_journal_links, parent, false)
+        // Return a new holder instance
+        return ViewHolder(journalView)
     }
 }
