9dc8582ad940b5a2f02584aecb534b056e0d89e9
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-16 16:16:19 +0200

    piccolo update
    sistemazione ui grafica

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
@@ -2,42 +2,25 @@ package com.pdm.meetgroups.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< Updated upstream
 import android.view.View
 import androidx.activity.viewModels
 import com.pdm.meetgroups.R
 import com.pdm.meetgroups.databinding.ActivitySignUpBinding
-import com.pdm.meetgroups.viewmodel.login.LoginViewModel
 import com.pdm.meetgroups.viewmodel.login.LoginViewModelImpl
 
 class SignUpActivity : AppCompatActivity() {
     private val loginVMImpl : LoginViewModelImpl by viewModels()
     private lateinit var binding: ActivitySignUpBinding
-=======
-import com.pdm.meetgroups.R
-import com.pdm.meetgroups.model.ModelImpl
-
-class SignUpActivity : AppCompatActivity() {
-    private var model = ModelImpl()
->>>>>>> Stashed changes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-<<<<<<< Updated upstream
-
         binding = ActivitySignUpBinding.inflate(layoutInflater)
     }
 
     fun signIn(view : View?) {
-        //change activity to sign in
     }
 
     fun signUp(view : View?) {
-        //controlli se le password sono uguali
-        loginVMImpl.signUpUser(binding.editTextEmail.toString(),
-            binding.editTextPasswordConfirm.toString())
-=======
->>>>>>> Stashed changes
     }
 }
