a99b6d2beeee91afd04c3d8fe2e4f98737496fc8
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-16 14:40:19 +0200

    piccole modifiche
    aggiunta modifica dinamica bio

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/EditProfileActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/EditProfileActivity.kt
@@ -1,27 +1,33 @@
 package com.pdm.meetgroups.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 //import androidx.preference.PreferenceFragmentCompat
 import com.pdm.meetgroups.R
+import kotlinx.android.synthetic.main.activity_edit_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class EditProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
-        /*if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }*/
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        editTextBio.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                bioTextView.text = s
+            }
+        })
     }
 
-    /*class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        }
-    }*/
 }
