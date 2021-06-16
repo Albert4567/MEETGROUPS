77b430f6c360675e1f78a50680bd0e9a9ee2ae75
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-16 15:53:31 +0200

    Update SignUpActivity.kt

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.pdm.meetgroups.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+<<<<<<< Updated upstream
 import android.view.View
 import androidx.activity.viewModels
 import com.pdm.meetgroups.R
@@ -12,10 +13,18 @@ import com.pdm.meetgroups.viewmodel.login.LoginViewModelImpl
 class SignUpActivity : AppCompatActivity() {
     private val loginVMImpl : LoginViewModelImpl by viewModels()
     private lateinit var binding: ActivitySignUpBinding
+=======
+import com.pdm.meetgroups.R
+import com.pdm.meetgroups.model.ModelImpl
+
+class SignUpActivity : AppCompatActivity() {
+    private var model = ModelImpl()
+>>>>>>> Stashed changes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+<<<<<<< Updated upstream
 
         binding = ActivitySignUpBinding.inflate(layoutInflater)
     }
@@ -28,5 +37,7 @@ class SignUpActivity : AppCompatActivity() {
         //controlli se le password sono uguali
         loginVMImpl.signUpUser(binding.editTextEmail.toString(),
             binding.editTextPasswordConfirm.toString())
+=======
+>>>>>>> Stashed changes
     }
 }
