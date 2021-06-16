bba11a1c12a561b7aab803f4aade1c5ff71b667f
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-16 23:38:48 +0200

    piccoli fix

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
@@ -1,64 +1,57 @@
 package com.pdm.meetgroups.view
 
-import android.R.attr.password
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.*
-import com.pdm.meetgroups.R
 import com.pdm.meetgroups.databinding.ActivitySignUpBinding
+import com.pdm.meetgroups.viewmodel.login.LoginViewModelImpl
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
-    private var mAuth: FirebaseAuth? = null
     private val TAG : String = "SignUpActivity"
+    private val loginVMImpl : LoginViewModelImpl by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(com.pdm.meetgroups.R.layout.activity_sign_up)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
-        mAuth = FirebaseAuth.getInstance()
-    }
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = mAuth!!.currentUser
-        updateUI(currentUser)
+        binding.signUpButton.setOnClickListener {
+            when {
+                TextUtils.isEmpty(editTextNickname.text.toString()) -> {
+                    editTextNickname.error = "inserisci un nickname"
+                    return@setOnClickListener
+                }
+                TextUtils.isEmpty(editTextEmail.text.toString()) -> {
+                    editTextEmail.error = "inserisci un email"
+                    return@setOnClickListener
+                }
+                TextUtils.isEmpty(editTextPassword.text.toString()) -> {
+                    editTextPassword.error = "inserisci una password"
+                    return@setOnClickListener
+                }
+                TextUtils.isEmpty(editTextPasswordConfirm.text.toString()) -> {
+                    editTextPasswordConfirm.error = "conferma la tua password"
+                    return@setOnClickListener
+                }
+                else -> loginVMImpl.signUpUser(
+                    binding.editTextEmail.toString(),
+                    binding.editTextPassword.toString()
+                )
+            }
+        }
     }
 
     fun signIn(view : View?) {
-    }
-
-    fun signUp(view : View?) {
-        signUpButton.setOnClickListener{
-            mAuth!!.createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
-                .addOnCompleteListener(
-                    this
-                ) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = mAuth!!.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            this@SignUpActivity, "Authentication failed.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        updateUI(null)
-                    }
-
-                    // ...
-                }
-        }
-    }
-    private fun updateUI(currentUser: FirebaseUser?) {
 
     }
+
+    //fun signUp(view : View?) {
+
+    //}
 }
