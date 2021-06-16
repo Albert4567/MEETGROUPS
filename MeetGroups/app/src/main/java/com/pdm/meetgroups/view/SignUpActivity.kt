b12a226dce1cbd06cec6bb032c37780d33e41d74
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-16 16:36:20 +0200

    Update SignUpActivity.kt
    stesura prova autenticazione

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
@@ -1,26 +1,64 @@
 package com.pdm.meetgroups.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.password
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import androidx.activity.viewModels
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.*
 import com.pdm.meetgroups.R
 import com.pdm.meetgroups.databinding.ActivitySignUpBinding
-import com.pdm.meetgroups.viewmodel.login.LoginViewModelImpl
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
 
 class SignUpActivity : AppCompatActivity() {
-    private val loginVMImpl : LoginViewModelImpl by viewModels()
     private lateinit var binding: ActivitySignUpBinding
+    private var mAuth: FirebaseAuth? = null
+    private val TAG : String = "SignUpActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
+        mAuth = FirebaseAuth.getInstance()
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
+        updateUI(currentUser)
     }
 
     fun signIn(view : View?) {
     }
 
     fun signUp(view : View?) {
+        signUpButton.setOnClickListener{
+            mAuth!!.createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = mAuth!!.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this@SignUpActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        updateUI(null)
+                    }
+
+                    // ...
+                }
+        }
+    }
+    private fun updateUI(currentUser: FirebaseUser?) {
+
     }
 }
