6e53ecda134475a815ceae85e6ba140d2968db92
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-18 15:31:03 +0200

    sistemazione grafica

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/EditProfileActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/EditProfileActivity.kt
@@ -9,25 +9,17 @@ import com.pdm.meetgroups.R
 import kotlinx.android.synthetic.main.activity_edit_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 
+/*TODO:Completare editprofileactivity, creare il viewmodel e connettere al db
+   uguale per profile fragment e aggiungere imagebutton per entrare in editprofile*/
+
 class EditProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        editTextBio.addTextChangedListener(object : TextWatcher {
 
-            override fun afterTextChanged(s: Editable) {}
-
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
-                bioTextView.text = s
-            }
-        })
     }
+
 
 }
