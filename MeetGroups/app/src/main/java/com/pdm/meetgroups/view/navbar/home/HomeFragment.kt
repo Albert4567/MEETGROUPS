7132d546adc183154af32588ac4a801c06d60b9f
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-19 16:41:45 +0200

    fix grafico
    sistemazione schermata iniziale Journal

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/home/HomeFragment.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/home/HomeFragment.kt
@@ -1,34 +1,28 @@
 package com.pdm.meetgroups.view.navbar.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.pdm.meetgroups.R
-import com.pdm.meetgroups.view.SignUpActivity
+import com.pdm.meetgroups.databinding.FragmentHomeBinding
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            text_home.text = it
-        })
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val html = "Benvenuto in <b>MeetGroups!</b><br>Quest'app ti permette di condividere esperienze e ricordi delle tue vacanze con i tuoi amici.<br>Clicca qui sopra per aprire un nuovo diario e iniziare la tua avventura. Una volta terminata la vacanza i tuoi diari rimarrano salvati nel tuo profilo.<br>Inoltre nella sezione <b>Maps</b> puoi trovare persone che stanno usando l'app nelle tue vicinanze!<br>Detto questo... <b>Buona Vacanza!</b>"
+        binding.textHome.text = HtmlCompat.fromHtml(html,HtmlCompat.FROM_HTML_MODE_LEGACY)
 
-        return view
+        return binding.root
     }
 }
