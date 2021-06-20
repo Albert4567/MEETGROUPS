18369881087856c63905c783dba2dee7f98bc3ff
Alberto <albibogoni@gmail.com>
2021-06-20 16:29:03 +0200

    Upload dell'immagine in EditJournal.

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/viewmodel/editjournal/EditJournalViewModel.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/viewmodel/editjournal/EditJournalViewModel.kt
@@ -1,9 +1,12 @@
 package com.pdm.meetgroups.viewmodel.editjournal
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import com.pdm.meetgroups.model.entities.UserContext
 import com.pdm.meetgroups.view.EditJournalActivity
+import com.pdm.meetgroups.view.PostCreationActivity
 
 interface EditJournalViewModel {
     fun getParticipants(): LiveData<ParticipantList>
@@ -12,11 +15,17 @@ interface EditJournalViewModel {
 
     fun getJournalTitle(): String?
 
+    fun getJournalImage(): Bitmap?
+
     fun updateJournalTitle(activity: EditJournalActivity)
+
+    fun startFileChooser(activity: EditJournalActivity)
+
+    fun updateJournalImage(data: Intent?, activity: EditJournalActivity)
 
     fun showAddParticipantActivity(context: Context)
 
-    fun removeParticipant(position: Int): Boolean
+    fun removeParticipant(position: Int, context: Context)
 
     fun closeJournal(context: Context)
 }
