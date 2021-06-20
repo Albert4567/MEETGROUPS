ccee466b1f6458dc6ab8363ad738c8e094ba6434
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-20 17:32:46 +0200

    aggiunto read only journal

---
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/ReadOnlyJournalActivity.kt
@@ -0,0 +1,12 @@
+package com.pdm.meetgroups.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pdm.meetgroups.R
+
+class ReadOnlyJournalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read_only_journal)
+    }
+}
