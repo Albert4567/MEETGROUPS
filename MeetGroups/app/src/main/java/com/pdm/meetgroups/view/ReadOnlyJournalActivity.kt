297a65a546e030eeea13badddc301f246e8cf724
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-06-22 10:37:44 +0200

    Update ReadOnlyJournalActivity.kt
    binding

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/ReadOnlyJournalActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/ReadOnlyJournalActivity.kt
@@ -3,10 +3,20 @@ package com.pdm.meetgroups.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pdm.meetgroups.R
+import com.pdm.meetgroups.databinding.ActivityPostCreationBinding
+import com.pdm.meetgroups.databinding.ActivityReadOnlyJournalBinding
+import com.pdm.meetgroups.view.adapter.PostImageListAdapter
 
 class ReadOnlyJournalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReadOnlyJournalBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_read_only_journal)
+
+        binding = ActivityReadOnlyJournalBinding.inflate(layoutInflater)
+        
+        return setContentView(binding.root)
     }
 }
