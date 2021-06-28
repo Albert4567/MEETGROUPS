b5706111f0c33fe5e7eb4c935f13bdf6bcdbd7a1
Alberto <albibogoni@gmail.com>
2021-06-28 10:43:12 +0200

    Modifiche minori.

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/ParticipantListAdapter.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/ParticipantListAdapter.kt
@@ -13,25 +13,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.pdm.meetgroups.R
 import com.pdm.meetgroups.viewmodel.editjournal.EditJournalViewModel
 
-// TODO(AB): Should i remove context formal parameter and pick the one in onCreateViewHolder
 class ParticipantListAdapter(
     private val editJournalVM: EditJournalViewModel,
     private val context: Context
     ): RecyclerView.Adapter<ParticipantListAdapter.ViewHolder>() {
 
     inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
-        lateinit var participantIMV: ImageView
-        lateinit var participantNameTV: TextView
-        lateinit var removeButton: Button
-        // Your holder should contain and initialize a member variable
-        // for any view that will be set as you render a row
-        // (AB): Here we should get widgets that belongs to row_post
-        init {
-            participantIMV = listItemView.findViewById(R.id.imv_participant_photo)
-            participantNameTV = listItemView.findViewById(R.id.tv_participant_name)
-            removeButton = listItemView.findViewById(R.id.btn_participant_remove)
-
-        }
+        var participantIMV: ImageView = listItemView.findViewById(R.id.imv_participant_photo)
+        var participantNameTV: TextView = listItemView.findViewById(R.id.tv_participant_name)
+        var removeButton: Button = listItemView.findViewById(R.id.btn_participant_remove)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -45,7 +35,6 @@ class ParticipantListAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val participant = editJournalVM.getParticipantBy(position)
 
-//        holder.participantIMV = editJournalVM.getParticipantBy(position)!!.getState().
         holder.participantNameTV.text = participant?.getState()?.nickname ?: "Unknown"
         holder.removeButton.setOnClickListener {
             editJournalVM.removeParticipant(position, context)
--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/PostListAdapter.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/PostListAdapter.kt
@@ -15,34 +15,23 @@ import com.pdm.meetgroups.viewmodel.journal.JournalViewModelImpl
 
 class PostListAdapter(val journalVM: JournalViewModelImpl): RecyclerView.Adapter<PostListAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
     inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
-        lateinit var postIMV: ImageView
-        lateinit var postNameTV: TextView
-        lateinit var postDescriptionTV: TextView
-
-        init {
-            postIMV = listItemView.findViewById(R.id.imv_postcard_photo)
-            postNameTV = listItemView.findViewById(R.id.tv_postcard_title)
-            postDescriptionTV = listItemView.findViewById(R.id.tv_postcard_description)
-        }
+        var postIMV: ImageView = listItemView.findViewById(R.id.imv_postcard_photo)
+        var postNameTV: TextView = listItemView.findViewById(R.id.tv_postcard_title)
+        var postDescriptionTV: TextView = listItemView.findViewById(R.id.tv_postcard_description)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
-        // Inflate the custom layout
         val postView = inflater.inflate(R.layout.row_post, parent, false)
-        // Return a new holder instance
+
         return ViewHolder(postView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // Set item views based on your views and data model
         val post = journalVM.getPostBy(position)
+
         if(post.images != null )
             holder.postIMV.setImageBitmap(post.images!!.first())
         holder.postNameTV.text = post.title
