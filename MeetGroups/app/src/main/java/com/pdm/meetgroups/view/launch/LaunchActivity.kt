5b4e123ccdaae7d599b8cfb138ef68292a193d17
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-07-02 16:59:41 +0200

    aggiunta splashactivity

---
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/launch/LaunchActivity.kt
@@ -0,0 +1,24 @@
+package com.pdm.meetgroups.view.launch
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.pdm.meetgroups.R
+import com.pdm.meetgroups.view.MainActivity
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@LaunchActivity,MainActivity::class.java))
+                finish()
+            },
+            2500
+        )
+    }
+}
