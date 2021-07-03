e0afd41f19db9dddaee831e7d286486b941458e3
Alberto <albibogoni@gmail.com>
2021-07-03 10:52:52 +0200

    Update PostImageListAdapter.kt

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/PostImageListAdapter.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/adapter/PostImageListAdapter.kt
@@ -1,14 +1,17 @@
 package com.pdm.meetgroups.view.adapter
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.pdm.meetgroups.R
-import com.pdm.meetgroups.viewmodel.postcreation.PostCreationViewModelImpl
+import com.pdm.meetgroups.viewmodel.journal.ViewModelAdapter
+import com.pdm.meetgroups.viewmodel.journal.ViewModelImageListAdapter
 
-class PostImageListAdapter(private val postCreationVM: PostCreationViewModelImpl) : RecyclerView.Adapter<PostImageListAdapter.ViewHolder>() {
+class PostImageListAdapter(private val viewModel: ViewModelImageListAdapter) : RecyclerView.Adapter<PostImageListAdapter.ViewHolder>() {
 
     inner class ViewHolder(listItemView: View) : RecyclerView.ViewHolder(listItemView) {
         var image: ImageView = listItemView.findViewById<ImageView>(R.id.row_post_image)
@@ -17,17 +20,20 @@ class PostImageListAdapter(private val postCreationVM: PostCreationViewModelImpl
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostImageListAdapter.ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
-        // Inflate the custom layout
         val postView = inflater.inflate(R.layout.row_post_image, parent, false)
-        // Return a new holder instance
+
         return ViewHolder(postView)
     }
 
     override fun onBindViewHolder(holder: PostImageListAdapter.ViewHolder, position: Int) {
-        holder.image.setImageURI(postCreationVM.getImageBy(position))
+        val image = viewModel.getImageBy(position)
+        if(image is Uri)
+            holder.image.setImageURI(viewModel.getImageBy(position) as Uri)
+        else
+            holder.image.setImageBitmap(viewModel.getImageBy(position) as Bitmap)
     }
 
     override fun getItemCount(): Int {
-        return postCreationVM.getImages().value!!.size
+        return viewModel.imageCount()
     }
 }
