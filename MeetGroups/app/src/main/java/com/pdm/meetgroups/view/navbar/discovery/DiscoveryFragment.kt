5e7418dd8e7d5bcfae82464c5f00e00547f3a87e
DamianoLampisti <63144846+DamianoLampisti@users.noreply.github.com>
2021-07-03 16:52:15 +0200

    fix caricamento immagini

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/discovery/DiscoveryFragment.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/navbar/discovery/DiscoveryFragment.kt
@@ -1,16 +1,22 @@
 package com.pdm.meetgroups.view.navbar.discovery
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.pdm.meetgroups.R
 import com.pdm.meetgroups.databinding.FragmentDiscoveryBinding
 import com.pdm.meetgroups.view.adapter.PostListAdapter
 import com.pdm.meetgroups.viewmodel.discovery.DiscoveryViewModel
+import kotlinx.android.synthetic.main.row_post.*
 
 class  DiscoveryFragment : Fragment() {
 
@@ -28,6 +34,9 @@ class  DiscoveryFragment : Fragment() {
             val tag = binding.discoverySearch.query.toString()
             discoveryVM.searchPostBy(tag)
         }
+
+        discoveryVM.getBlankBitmap(requireContext())
+        discoveryVM.loadPosts()
 
         discoveryVM.getPosts().observe(viewLifecycleOwner, Observer {
             binding.searchRecyclerView.layoutManager = LinearLayoutManager(requireActivity())
