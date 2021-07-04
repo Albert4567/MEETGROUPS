8851493d7d0a3f60c4841be6f0e1cd15e1005912
mbarbaro09 <mbarbaro@studenti.uninsubria.it>
2021-07-04 12:07:50 +0200

    corretta Conferma Password

--- MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
+++ MeetGroups/app/src/main/java/com/pdm/meetgroups/view/SignUpActivity.kt
@@ -6,6 +6,7 @@ import android.text.TextUtils
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.pdm.meetgroups.R
 import com.pdm.meetgroups.databinding.ActivitySignUpBinding
 import com.pdm.meetgroups.viewmodel.login.LoginViewModelImpl
 import kotlinx.android.synthetic.main.activity_sign_up.*
@@ -35,28 +36,33 @@ class SignUpActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
-
-    //TODO finish check for confirm password
+    
     private fun onSignUpClick() {
         var notEmpty = true
+        var notEqualPasswords = true
         if (TextUtils.isEmpty(editTextNickname.text.toString())) {
-            editTextNickname.error = "inserisci un nickname"
+            editTextNickname.error = getString(R.string.addNickname)
             notEmpty = false
         }
         if (TextUtils.isEmpty(editTextEmail.text.toString())) {
-            editTextEmail.error = "inserisci un email"
+            editTextEmail.error = getString(R.string.addEmail)
             notEmpty = false
         }
         if (TextUtils.isEmpty(editTextPassword.text.toString())) {
-            editTextPassword.error = "inserisci una password"
+            editTextPassword.error = getString(R.string.addPassword)
             notEmpty = false
         }
         if (TextUtils.isEmpty(editTextPasswordConfirm.text.toString())) {
-            editTextPasswordConfirm.error = "conferma la tua password"
+            editTextPasswordConfirm.error = getString(R.string.confirmPassword)
             notEmpty = false
         }
 
-        if (notEmpty) {
+        if (editTextPassword.text.toString() != editTextPasswordConfirm.text.toString()) {
+            editTextPasswordConfirm.error = getString(R.string.passwordsNotEquals)
+            notEqualPasswords = false
+        }
+
+        if (notEmpty && notEqualPasswords) {
             signUpButton.visibility = View.INVISIBLE
             progressBarUp.visibility = View.VISIBLE
             loginVMImpl.signUpUser(
